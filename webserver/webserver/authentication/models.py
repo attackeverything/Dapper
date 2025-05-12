56f144649c976384fbcab61be69f4f0219674498
attackeverything <bagdubois@gmail.com>
2025-05-12 10:42:55 -0400

    Moving manage.py up

--- webserver/webserver/authentication/models.py
+++
@@ -1,5 +0,0 @@
-from django.db import models
-from django.contrib.auth.models import AbstractUser
-
-class CustomUser(AbstractUser):
-    academic_affiliation = models.CharField(max_length=255, blank=True, null=True)
--- webserver/webserver/submissions/utils.py
+++
@@ -1,8 +0,0 @@
-from django.core.exceptions import ValidationError
-import os
-
-def validate_file_extension(file):
-    valid_extensions = ['.mat', '.m']
-    extension = os.path.splitext(file.name)[1]
-    if extension.lower() not in valid_extensions:
-        raise ValidationError('File not supported. Please upload a MATLAB (.mat or .m) file.')
