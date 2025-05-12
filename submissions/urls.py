95bd379b6af75e1d2a31029f909b701b8f631055
attackeverything <bagdubois@gmail.com>
2025-05-12 11:37:56 -0400

    Update urls.py

--- submissions/urls.py
+++ submissions/urls.py
@@ -3,7 +3,4 @@ from . import views
 
 urlpatterns = [
     path("", views.index, name="index"),
-    path("submissionsPage", views.submissionsPage, name="submissionsPage"),
-    path('submissionsPage/<str:submission_id>/', views.submission_detail, name='submission'),
-    path('downloadAllFiles/<str:submission_id>/', views.download_all_files, name='downloadAllFiles'),
 ]
