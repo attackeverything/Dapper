e901f7293bc3138482ddaea114d623d5fc9b4122
attackeverything <bagdubois@gmail.com>
2025-05-12 11:36:01 -0400

    Update views.py

--- submissions/views.py
+++ submissions/views.py
@@ -1,1 +1,30 @@
+from django.http import HttpRequest, HttpResponse
+from django.shortcuts import render, redirect
+from .models import Submission, SubmissionStatus
+from .forms import FileUploadForm
+from django.utils import timezone
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import get_user_model
+from django.db.models import Exists, OuterRef
+from django.shortcuts import render, get_object_or_404
+from django.http import Http404
+from zipfile import ZipFile
 
+def index(request: HttpRequest):
+    if request.method == 'POST':
+        form = FileUploadForm(request.POST, request.FILES)
+        if form.is_valid():
+            submission: Submission = form.save(commit=False)
+            submission.status = SubmissionStatus.objects.get(name="pending")
+            submission.submitted_at = timezone.now()
+            submission.user = request.user
+            submission.save()
+
+            # once submission is successfulled stored in db,
+            # create a task to execute it
+
+            run_submission.delay(submission.id)
+            return redirect('submissionsPage')
+    else:
+        form = FileUploadForm()
+    return render(request, "submissions/index.html", {"form": form})
