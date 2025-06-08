c3c8c6c844d95d421c6742df073c13d1bf5c5ae4
attackeverything <bagdubois@gmail.com>
2025-06-07 23:38:14 -0400

    Fixing calendar

--- home/models.py
+++ home/models.py
@@ -5,6 +5,6 @@ class Post(models.Model):
     title = models.TextField(default='Social Media Post')
     content = models.TextField()
     platform = models.CharField(max_length=20)
-    image = models.ImageField(upload_to='media/images/', null=True, blank=True)
+    image = models.ImageField(upload_to='images/')
     scheduled_time = models.DateTimeField()
     status = models.CharField(max_length=10, choices=[('pending', 'Pending'), ('approved', 'Approved'), ('rejected', 'Rejected')])
